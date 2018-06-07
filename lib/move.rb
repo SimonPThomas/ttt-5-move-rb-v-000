e6d16827efc6266cda130004b09688b13f61e545
Simon Thomas <simonpthomas89@gmail.com>
2018-06-07 19:27:58 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,6 @@ end
 
 
 def input_to_index (user_input)
-    ('user_input - 1')
+  converted_input =  ('user_input - 1')
 puts "#{converted_input}"
 end
-  converted_input = input_to_index(user_input)
